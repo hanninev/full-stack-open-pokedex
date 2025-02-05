66611e9ea5348e05f0a426552825e2e6d4b0d6e0
Ville <ville.hanninen@helsinki.fi>
2025-02-05 18:41:24 +0200

    11.12

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
-app.listen(PORT +1, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })

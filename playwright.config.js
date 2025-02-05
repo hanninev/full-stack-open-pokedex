3f14bf7c13a26d1efe8bc81019c885a4d8d8e7bd
Ville <ville.hanninen@helsinki.fi>
2025-02-05 17:26:57 +0200

    11.9 fix5

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:8081',
+    url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
   },
 });
